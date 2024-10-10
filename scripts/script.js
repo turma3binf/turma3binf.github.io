985c839edaa9d0305d7fa48a7864ca9f62a450c5
turma3binf <kasejohn71@gmail.com>
2024-10-10 17:36:06 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,57 @@
 const divJogo = document.getElementById("telaJogo");
 const fundoJogo = document.getElementById("fundoJogo");
-const hudTemp = document.getElementById("hudTemp");
 
+//comodos
 var comodos = ["Titulo", "Cozinha"];
-var comodoMapas = ["#telaTitulo", "#mapaCozinha"]
-var comodoImagens = ["/assets/titulo.png", "/assets/cozinha.png"]
-var comodoAtual = comodos[1];
+var comodoMapas = ["#telaTitulo", "#mapaCozinha"];
+var comodoImagens = ["/assets/titulo.jpeg", "/assets/cozinha.png"]
+var comodoAtual = comodos[0];
 
+//interface
+const interfaceHUD = document.getElementById("interfaceJogo");
+const adesivoHUD = document.getElementById("adesivo");
+
+const consumoAtual = document.getElementById("consumoAtual");
+const consumoInicial = document.getElementById("consumoInicial");
+const economiaTotal = document.getElementById("economiaTotal");
+
+//quartos
 const cozinhaMapa = document.getElementsByName("mapaCozinha");
-var cozinhaObjetos = [["geladeira", "Geladeira", 250], ["microondas", "Micro-ondas", 750], ["fogao", "Fogão", 1500], ["loucas", "Lava-louças", 1500]];
-var cozinhaProgresso = [];
+var cozinhaObjetos = [["geladeira", "Geladeira", 25, true], ["microondas", "Micro-ondas", 75, true], ["fogao", "Fogão", 150, true], ["loucas", "Lava-louças", 150, true]];
+var cozinhaProgresso = 0;
+
+var jogadorComodo = 0;
+var jogadorProgresso = [cozinhaProgresso];
 
 function mudarTela(mapa){
 	fundoJogo.setAttribute('usemap', comodoMapas[mapa]);
 	console.log(fundoJogo);
 	fundoJogo.src = comodoImagens[mapa];
+	
+	jogadorComodo = mapa;
+	console.log(jogadorComodo);
+	comodoAtual = comodos[mapa];
+	
+	if(mapa != 0){ interfaceHUD.style.visibility = "visible"; }
 }
 
 function objetoClicado(objetoNome){
 	var objetoAtual = cozinhaObjetos[objetoNome];
 	
-	let resultado = '<br>' + objetoAtual[1] + ' -> ' + objetoAtual[2] + ' Kwh';
-	hudTemp.innerHTML += resultado.toString();
+	if(!objetoAtual[3]){
+		console.log("Não.");
+		return;
+	} else {
+		objetoAtual[3] = false;
+		jogadorProgresso[jogadorComodo - 1] += objetoAtual[2];
+
+		let resultado = jogadorProgresso[jogadorComodo - 1];
+		consumoAtual.innerHTML = resultado.toString();
+	}
 }
 
+let eventoToque = 'ontouchstart' in window ? 'touchstart' : 'click';
+
 if(screen.availHeight > screen.availWidth){
-    alert("Por favor vire o celular!");
+	//
 }
